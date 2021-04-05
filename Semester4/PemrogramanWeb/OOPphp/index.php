4908a90d8892fab1d65aebb2bb023978f401c8e0
zhirobyte <57735877+zhirobyte@users.noreply.github.com>
2021-04-05 15:39:28 +0700

    Add files via upload

---
+++ Semester4/PemrogramanWeb/OOPphp/index.php
@@ -0,0 +1,13 @@
+<?php
+   require_once('class.kucing.php');
+   require_once('class.burung.php');
+   require_once('class.monyet.php');
+   require_once('class.hewan.php'); 
+ 
+ 
+  $objHewan = new Hewan();
+   
+
+  
+  
+?>
